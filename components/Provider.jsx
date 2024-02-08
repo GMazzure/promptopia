8091d809e90feaf4347dc41598fda7f8598fe182
gustavo <gustavomazzure@gmail.com>
2024-02-08 08:52:17 -0300

    Google Authentication, Top bar, Profile page, create post, home page, feed

---
+++ components/Provider.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import  { SessionProvider } from 'next-auth/react'
+
+const Provider = ({ children, session}) => {
+  return (
+    <SessionProvider sesion={session}>
+      {children}
+    </SessionProvider>
+  )
+}
+
+export default Provider
